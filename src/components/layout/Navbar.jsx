323bbbaa60cabbac99e4f6f1accb39f5f4a01c3f
Umme Shawrin <ummeshawrin@gmail.com>
2026-03-25 16:20:07 +0600

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
+ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router";
 import { FaSearch } from "react-icons/fa";
 import { FaRegUser } from "react-icons/fa";
 import { CiShoppingCart } from "react-icons/ci";
-import { ChevronDown } from "lucide-react";
 import { FaBars } from "react-icons/fa";
-import { FaChevronRight } from "react-icons/fa";
 import { useCart } from "../../context/CartContext";
 
 const Navbar = () => {
@@ -13,58 +11,6 @@ const Navbar = () => {
   const { totalItems } = useCart();
   const user = JSON.parse(localStorage.getItem("user"));
   const navigate = useNavigate();
-
-  const categories = [
-    {
-      title: "phone",
-      to: "",
-      children: [
-        { title: "samsung", to: "" },
-        { title: "vivo", to: "" },
-        { title: "nokia", to: "" },
-        { title: "iphone", to: "" },
-      ],
-    },
-    {
-      title: "watch",
-      to: "",
-      children: [
-        { title: "samsung", to: "" },
-        { title: "vivo", to: "" },
-        { title: "iphone", to: "" },
-      ],
-    },
-    {
-      title: "laptop",
-      to: "",
-      children: [
-        { title: "samsung", to: "" },
-        { title: "vivo", to: "" },
-        { title: "iphone", to: "" },
-        { title: "nokia", to: "" },
-      ],
-    },
-    {
-      title: "tws",
-      to: "",
-      children: [
-        { title: "iphone", to: "" },
-        { title: "samsung", to: "" },
-        { title: "vivo", to: "" },
-        { title: "nokia", to: "" },
-      ],
-    },
-    {
-      title: "panjabi",
-      to: "",
-      children: [
-        { title: "nokia", to: "" },
-        { title: "samsung", to: "" },
-        { title: "iphone", to: "" },
-        { title: "vivo", to: "" },
-      ],
-    },
-  ];
 
   return (
     <header>
@@ -142,33 +88,6 @@ const Navbar = () => {
         </div>
       </nav>
 
-      <div>
-        <div className="py-4 border-y border-secondary hidden md:block">
-          <div className="container flex gap-6">
-            {categories.map((items) => (
-              <div className="relative group" key={items.title}>
-                <button className="px-4 py-2 bg-theme rounded-full flex items-center gap-1 capitalize">
-                  {items.title} <ChevronDown className="w-4 h-4" />
-                </button>
-
-                <ul className="absolute left-0 top-full mt-2 hidden group-hover:block bg-theme shadow rounded z-50">
-                  {items.children.map((child) => (
-                    <li key={child.title}>
-                      <Link
-                        to={`/${child.title.trim().toLowerCase()}`}
-                        className="block px-4 py-2 hover:bg-theme"
-                      >
-                        {child.title}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-
       {sidebar && (
         <div
           onClick={() => setSidebar(false)}
@@ -178,29 +97,6 @@ const Navbar = () => {
             onClick={(e) => e.stopPropagation()}
             className="w-4/5 sm:w-3/5 bg-theme h-full p-4"
           >
-            <ul className="space-y-4 text-primary font-bold text-lg mb-5 pb-5 border-b border-secondary">
-              {categories.map((items) => (
-                <li key={items.title}>
-                  <div className="flex justify-between items-center">
-                    <Link to={items.to}>{items.title}</Link>
-                    <button>
-                      <FaChevronRight />
-                    </button>
-                  </div>
-
-                  <ul className="hidden font-semibold text-base pl-2 space-y-2 mt-2">
-                    {items.children.map((child) => (
-                      <li key={child.title}>
-                        <Link to={`/${child.title.trim().toLowerCase()}`}>
-                          {child.title}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-              ))}
-            </ul>
-
             {user ? (
               <div
                 onClick={() => {
